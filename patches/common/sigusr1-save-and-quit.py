92ee739595a8bc06ad83b77d24cf7c27796eabef
SundownerSport <kilchisblue@aol.com>
2026-03-29 16:58:11 -0700

    Fix SaveState::Status name collision in SIGUSR1 patch
    Use auto lambda parameters to avoid conflict with Status macro
    from system headers.

--- patches/common/sigusr1-save-and-quit.py
+++ patches/common/sigusr1-save-and-quit.py
@@ -84,9 +84,7 @@ NEW4 = '''\t\tif (g_QuitRequested || g_RestartRequested)
 \t\t\t\tg_saveAndQuit = 0;
 \t\t\t\tif (PSP_IsInited() && GetUIState() == UISTATE_INGAME) {
 \t\t\t\t\tSaveState::SaveSlot(SaveState::GetGamePrefix(g_paramSFO), g_Config.iCurrentStateSlot,
-\t\t\t\t\t\t[](SaveState::Status status, std::string_view msg) {
-\t\t\t\t\t\t\tg_QuitRequested = true;
-\t\t\t\t\t\t});
+\t\t\t\t\t\t[](auto, auto) { g_QuitRequested = true; });
 \t\t\t\t} else {
 \t\t\t\t\tg_QuitRequested = true;
 \t\t\t\t}
